2cacc953955b5cb3a16de817aeb211eb1f6f1d01
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2022-01-21 14:32:36 -0600

    new imgs and removed old imgs. Css changes

--- scripts/home.js
+++ scripts/home.js
@@ -1,26 +1,31 @@
-// schedule modal
-const scheduleModalBtn = document.querySelector('.modal-trig').addEventListener('click', () => {
-  document.querySelector('.schedule-modal').style.display = 'flex';
-})
-const closeSchModal = document.querySelector('.close-sch-modal').addEventListener('click', () => {
-  document.querySelector('.schedule-modal').style.display = 'none';
-})
-// select all images in home gallery
-const galleryModal = document.querySelector('.gallery-modal');
-const galleryModalImg = document.querySelector('.gallery-modal img');
-const closeGalBtn = document.querySelector('.close-gal-modal').addEventListener('click', () => {
-  galleryModal.style.display = 'none'
-})
-const galleryHomeImgs = document.querySelectorAll('#masonry-gallery .container .masonry .item img')
+/** @format */
 
+// schedule modal
+const scheduleModalBtn = document
+  .querySelector(".modal-trig")
+  .addEventListener("click", () => {
+    document.querySelector(".schedule-modal").style.display = "flex";
+  });
+const closeSchModal = document
+  .querySelector(".close-sch-modal")
+  .addEventListener("click", () => {
+    document.querySelector(".schedule-modal").style.display = "none";
+  });
+// select all images in home gallery
+const galleryModal = document.querySelector(".gallery-modal");
+const galleryModalImg = document.querySelector(".gallery-modal img");
+const closeGalBtn = document
+  .querySelector(".close-gal-modal")
+  .addEventListener("click", () => {
+    galleryModal.style.display = "none";
+  });
+const galleryHomeImgs = document.querySelectorAll(
+  "#masonry-gallery .container .masonry img"
+);
 
 galleryHomeImgs.forEach((img) => {
-  img.addEventListener('click', () => {
-    galleryModal.style.display = 'flex';
+  img.addEventListener("click", () => {
+    galleryModal.style.display = "flex";
     galleryModalImg.src = img.src;
-
-  })
+  });
 });
-
-
-let arr = ['1', '2', '3'];
