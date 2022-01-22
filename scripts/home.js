603666d976ee1b00d0c4db8b6975723edac6a485
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2022-01-21 19:12:04 -0600

    added a swiper to the modal for the gallery

--- scripts/home.js
+++ scripts/home.js
@@ -24,8 +24,82 @@ const galleryHomeImgs = document.querySelectorAll(
 );
 
 galleryHomeImgs.forEach((img) => {
-  img.addEventListener("click", () => {
-    galleryModal.style.display = "flex";
-    galleryModalImg.src = img.src;
-  });
+  img.addEventListener("click", renderModalSwiper);
 });
+
+// create new swiper logic
+function renderModalSwiper(img) {
+  const body = document.querySelector("body");
+  // create background modal
+  const backgroundOverlay = document.createElement("div");
+  backgroundOverlay.className = "modal-overlay";
+
+  // create swiper container
+  const swiperContainer = document.createElement("div");
+  swiperContainer.className = "swiper-container swiperModal";
+
+  // create swiper wrapper
+  const swiperWrapper = document.createElement("div");
+  swiperWrapper.className = "swiper-wrapper";
+  // create swiper pagination
+  const swiperPagination = document.createElement("div");
+  swiperPagination.className = "swiper-pagination-modal";
+
+  // create close button
+  const closeModalEl = document.createElement("div");
+  closeModalEl.className = "close-swiper-modal";
+  const closeModalInnerEl = document.createElement("div");
+  closeModalEl.appendChild(closeModalInnerEl);
+
+  // create a new slide for each gallery img and append it to the parent container
+  let slides = "";
+  galleryHomeImgs.forEach((img) => {
+    slides += `<div class="swiper-slide"><img src='${img.src}'></div>`;
+    swiperWrapper.innerHTML = slides;
+  });
+
+  // append all
+  swiperContainer.appendChild(swiperWrapper);
+  backgroundOverlay.appendChild(closeModalEl);
+  backgroundOverlay.appendChild(swiperPagination);
+  backgroundOverlay.appendChild(swiperContainer);
+
+  // append to overlay
+  body.appendChild(backgroundOverlay);
+
+  function returnNewSwiper() {
+    // find index and set it to swiper initialSlide value
+    const thisImgSrc = img.target.currentSrc;
+    let thisIndex;
+    galleryHomeImgs.forEach((img, index) => {
+      if (img.src === thisImgSrc) {
+        thisIndex = index;
+      } else {
+        return;
+      }
+    });
+    const modalSwiper = new Swiper(".swiperModal", {
+      // should be a number
+      initialSlide: thisIndex,
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination-modal",
+        type: "fraction",
+      },
+      // navigation: {
+      //   nextEl: ".swiper-button-next",
+      //   prevEl: ".swiper-button-prev",
+      // },
+    });
+  }
+  // close modal logic
+  closeModalEl.addEventListener("click", () => {
+    body.removeChild(backgroundOverlay);
+  });
+  return returnNewSwiper();
+}
