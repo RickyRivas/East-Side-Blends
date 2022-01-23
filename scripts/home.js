8f4817287975b0d32dc78c1e3404c73cb3c89a09
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2022-01-22 22:20:56 -0600

    new footer redesign

--- scripts/home.js
+++ scripts/home.js
@@ -1,13 +1,53 @@
 /** @format */
 
-// schedule modal
-const scheduleModalBtn = document
+const trigger = document
   .querySelector(".modal-trig")
-  .addEventListener("click", () => {
-    document.querySelector(".schedule-modal").style.display = "flex";
+  .addEventListener("click", renderScheduleModal);
+
+function renderScheduleModal() {
+  const body = document.querySelector("body");
+  const modalBackground = document.createElement("div");
+  modalBackground.className = "schedule-modal";
+  modalBackground.innerHTML = `
+     <div class="table">
+        <div class="close-sch-modal"><span></span></div>
+        <div class="heading">
+          <h3>Shop Schedule</h3>
+        </div>
+        <ul>
+          <li>
+            <p>Monday:</p>
+            <p>7am:5pm</p>
+          </li>
+          <li>
+            <p>Tuesday:</p>
+            <p>7am:5pm</p>
+          </li>
+          <li>
+            <p>Wednesday:</p>
+            <p>7am:5pm</p>
+          </li>
+          <li>
+            <p>Thursday:</p>
+            <p>7am:5pm</p>
+          </li>
+          <li>
+            <p>Friday:</p>
+            <p>7am:5pm</p>
+          </li>
+          <li>
+            <p>Saturday:</p>
+            <p>7am:5pm</p>
+          </li>
+          <li>
+            <p>Sunday:</p>
+            <p>7am:5pm</p>
+          </li>
+        </ul>
+      </div>
+  `;
+  body.append(modalBackground);
+  document.querySelector(".close-sch-modal").addEventListener("click", () => {
+    body.removeChild(modalBackground);
   });
-const closeSchModal = document
-  .querySelector(".close-sch-modal")
-  .addEventListener("click", () => {
-    document.querySelector(".schedule-modal").style.display = "none";
-  });
+}
